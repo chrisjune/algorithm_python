6295a238f75bbab93cffddc5c3d1d954cf6dcccf
chrisjune <chrisjune@gmail.com>
2022-10-03 23:11:11 +0900

    edit make_to_one

--- dynamic_programming/make_to_one.py
+++ dynamic_programming/make_to_one.py
@@ -1,16 +1,18 @@
 x = int(input())
 
-cache = [0] * 30001
+cache = [0] * (x + 1)
+
 for i in range(2, x + 1):
-    # 1 뺀경우
-    cache[i] = cache[i - 1] + 1
-    # 2. 나눈경우
-    if cache[i] % 2 == 0:
-        cache[i] = min(cache[i], cache[i // 2] + 1)
+    candidate = []
 
-    if cache[i] % 3 == 0:
-        cache[i] = min(cache[i], cache[i // 3] + 1)
+    if i - 1 > 0:
+        candidate.append(cache[i - 1])
+    if i % 2 == 0:
+        candidate.append(cache[i // 2])
+    if i % 3 == 0:
+        candidate.append(cache[i // 3])
+    if i % 5 == 0:
+        candidate.append(cache[i // 5])
+    cache[i] = min(candidate) + 1
 
-    if cache[i] % 5 == 0:
-        cache[i] = min(cache[i], cache[i // 5] + 1)
 print(cache[x])
